7f606546456905cd64389876f628fd5677bf4125
Deepthi-5 <97046296+Deepthi-5@users.noreply.github.com>
2022-01-19 21:34:24 +0530

    Add files via upload

---
+++ Positive Numbers.py
@@ -0,0 +1,13 @@
+list1=[12,-7,5,64,-14]
+l=[]
+for i in list1:
+    if i>=0:
+        l.append(i)
+print(l)
+
+list2=[12,14,-95,3]
+a=[]
+for i in list2:
+    if i>=0:
+        a.append(i)
+print(a)
